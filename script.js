cf0551dc913e15c13c03329764d8c64f92cf412d
MD KAMRAN KHAN <124767601+kamrankhan0001@users.noreply.github.com>
2023-05-11 19:58:34 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,30 @@
+var form = document.querySelector("form");
+
+form.addEventListener("submit", function (e) {
+  e.preventDefault();
+  var search = form.querySelector("input[type=search]");
+//   search.value = "site:css-tricks.com " + search.value;
+  form.submit();
+});
+
+function myFunc() {
+    window.location.assign("https://chat.openai.com/auth/login")
+    }
+    
+function myFuncti() {
+     window.location.assign("https://github.com/")
+        }
+
+  function myFun() {
+ window.location.assign("https://acciojob.com/scholarship-test.html?source=google&campaign=IDP_Brand_All-MT_PAN-India&medium=SEM&content=AccioSAT&gclid=Cj0KCQjwpPKiBhDvARIsACn-gzBUOP3vuvSJKJq_jjR2QmeZgXRYKxgxnMwuvbwc2wUcF4q4N5lGH5IaAghREALw_wcB")
+  }
+
+function myFunct() {
+ window.location.assign("https://www.linkedin.com/feed/")
+
+}
+function myFunction() {
+    window.location.assign("https://www.w3schools.com")
+     }
+
+  
